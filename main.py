493402f5051afe1aa365899c40a7225787e6c714
Yudai Nakata <nakata@dragon.t.u-tokyo.ac.jp>
2026-08-16 15:25:02 +0900

    add buttons

--- main.py
+++ main.py
@@ -18,10 +18,20 @@ class MyWidget(QtWidgets.QWidget):
         self.layout.addWidget(self.button)
 
         self.button.clicked.connect(self.magic)
+        self.button_pen = self.generate_button("Drive Pen")
+        self.button_servo_on = self.generate_button("Servo On")
+        self.button_servo_off = self.generate_button("Servo Off")
 
     @QtCore.Slot()
     def magic(self):
         self.text.setText(random.choice(self.hello))
+
+    def generate_button(self, name, xpos=100, ypos=100, width=100, height=50):
+        button = QtWidgets.QPushButton()
+        button.setObjectName(name)
+        button.setText(name)
+        self.layout.addWidget(button)
+        return button
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
